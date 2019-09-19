3cf2d3bb692fd29a230f166d690eb205a1f4a8c2
Heedo Kim <devheedoo@gmail.com>
2019-09-19 15:45:44 +0900

    8. App.js 수정해서 테스트해보기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Decorator that passes on a 'message' property to a class.
+const addMessage = (str) => (component) => {
+  component.prototype.message = str
 }
 
+@addMessage('Hello world!')
+class App extends React.PureComponent {
+  render() {
+    return <div>{ this.message }</div>
+  }
+}
 export default App;
